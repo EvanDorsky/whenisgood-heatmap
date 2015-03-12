c5b4ab916e14eddf14c29edea8ce0c2cc0de9922
Evan Dorsky <evan.dorsky@icloud.com>
2015-03-11 20:53:47 -0400

    Add clarifying alert

--- heat-pack/click.js
+++ heat-pack/click.js
@@ -1,3 +1,4 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
+	alert("Click on this when you're viewing whenisgood results in the active tab.");
 	chrome.tabs.executeScript(null, {file: 'heatmap.js'});
 });
